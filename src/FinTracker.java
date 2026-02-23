016f64f214fa622c3033e07f895f3e104df2c187
Ney Adrian <neyadrian2018@gmail.com>
2026-02-23 15:59:19 -0300

    contrutor em fin tracker

--- src/FinTracker.java
+++ src/FinTracker.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FinTracker {
+    private List<Transacao> transacoes;
+
+    public FinTracker(List<Transacao> transacoes) {
+        this.transacoes = new ArrayList<>();
+    }
+
+
 }
