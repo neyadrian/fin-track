4e11b2dd9c754f2369f23abe9c64b68d84bc2bfc
Ney Adrian <neyadrian2018@gmail.com>
2026-02-23 15:50:26 -0300

    construtor em transação mensal

--- src/TransacaoMensal.java
+++ src/TransacaoMensal.java
@@ -1,2 +1,15 @@
-public class TransacaoMensal {
+import java.time.LocalDate;
+
+public class TransacaoMensal extends Transacao {
+    private int mesesRecorrencia;
+
+    public TransacaoMensal(String descricao, double valor, LocalDate data, String tipo, int mesesRecorrencia) {
+        super(descricao, valor, data, tipo);
+
+        if(mesesRecorrencia <= 0) {
+            throw new IllegalArgumentException("Meses de recorrência deve ser maior que zero");
+        }
+
+        this.mesesRecorrencia = mesesRecorrencia;
+    }
 }
