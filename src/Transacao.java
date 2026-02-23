71d6be98cf8292b4dc487032c086422b5868d5aa
Ney Adrian <neyadrian2018@gmail.com>
2026-02-23 15:45:27 -0300

    getters e setters em transação

--- src/Transacao.java
+++ src/Transacao.java
@@ -10,16 +10,47 @@ public class Transacao {
     public Transacao(String descricao, double valor, LocalDate data, String tipo) {
 
         if(valor <= 0) {
-            throw new IllegalArgumentException("Valor deve ser maior que zero!");
+            throw new IllegalArgumentException("Valor deve ser maior que zero");
         }
 
-        if (!tipo.equalsIgnoreCase("RECEITA") && !tipo.equalsIgnoreCase("DISPESA")) {
-            throw new IllegalArgumentException("Tipo deve ser RECEITA ou DESPESA!");
+        if (!tipo.equalsIgnoreCase("RECEITA") && !tipo.equalsIgnoreCase("DESPESA")) {
+            throw new IllegalArgumentException("Tipo deve ser RECEITA ou DESPESA");
         }
 
         this.descricao = descricao;
         this.valor = valor;
         this.data = data;
         this.tipo = tipo.toUpperCase();
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setValor(double valor) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("Valor deve ser maior que zero");
+        }
+        this.valor = valor;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
     }
 }
