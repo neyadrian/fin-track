f2822a6f3fa4272fcbaa918809ac807b6a241ed0
Ney Adrian <neyadrian2018@gmail.com>
2026-02-23 15:42:55 -0300

    update em construtor da transação

--- src/Transacao.java
+++ src/Transacao.java
@@ -8,9 +8,18 @@ public class Transacao {
     private String tipo;
 
     public Transacao(String descricao, double valor, LocalDate data, String tipo) {
+
+        if(valor <= 0) {
+            throw new IllegalArgumentException("Valor deve ser maior que zero!");
+        }
+
+        if (!tipo.equalsIgnoreCase("RECEITA") && !tipo.equalsIgnoreCase("DISPESA")) {
+            throw new IllegalArgumentException("Tipo deve ser RECEITA ou DESPESA!");
+        }
+
         this.descricao = descricao;
         this.valor = valor;
         this.data = data;
-        this.tipo = tipo;
+        this.tipo = tipo.toUpperCase();
     }
 }
