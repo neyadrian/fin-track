9742e60c2b967cda29a514639a37a911b5475f99
Ney Adrian <neyadrian2018@gmail.com>
2026-02-23 16:07:32 -0300

    método adicionar e listar trasação em fin tracker

--- src/FinTracker.java
+++ src/FinTracker.java
@@ -8,5 +8,25 @@ public class FinTracker {
         this.transacoes = new ArrayList<>();
     }
 
+    public void adicionarTransacao(Transacao transacao) {
+        if(transacao == null) {
+            throw new IllegalArgumentException("Transação não pode ser nula");
+        }
+        transacoes.add(transacao);
+        System.out.println("Transação adicionada com sucesso!");
+    }
+
+    public void listarTransacoes() {
+        if(transacoes.isEmpty()) {
+            System.out.println("\n Nenhuma transação registrada.");
+            return;
+        }
+
+        System.out.println("\n===== LISTA DE TRANSAÇÕES =====");
+        for(int i = 0; i < transacoes.size(); i++) {
+            System.out.printf("%d. %s%n", i + 1, transacoes.get(i));
+        }
+        System.out.println("=====================================\n");
+    }
 
 }
